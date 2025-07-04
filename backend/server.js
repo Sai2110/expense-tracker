40abe0cb4570a5af8acbec334199f6f73668d561
Sai2110 <saimehetre2002@gmail.com>
2025-07-04 11:13:30 +0530

    Initial commit - fullstack project WIP

---
+++ backend/server.js
@@ -0,0 +1,21 @@
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+
+const app = express();
+
+
+//Middleware to handle CORS
+app.use(
+    cors({
+        origin: process.env.CLIENT_URL || "*",
+        methods: ["GET", "POST", "DELETE" , "PUT"],
+        allowedHeaders: ["Content-Type", "Autorization"],
+    })
+);
+
+app.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
---
+++ frontend/expense-tracker/src/App.jsx
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import{
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate , 
+} from "react-router-dom";
+
+import Login from "./pages/Auth/Login.jsx"
+import SignUp from "./pages/Auth/SignUp.jsx"
+import Home from "./pages/Dashboard/Home.jsx"
+import Income from "./pages/Dashboard/Income.jsx"
+import Expense from "./pages/Dashboard/Expense.jsx"
+
+const App = () => {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<Root />} />
+          <Route path="/login" exact element={<Login />} />
+          <Route path="/signUp" exact element={<SignUp />} />
+          <Route path="/dashboard" exact element={<Home />} />
+          <Route path="/income" exact element={<Income />} />
+          <Route path="/expense" exact element={<Expense />} />
+        </Routes>
+      </Router>
+    </div>
+  )
+};
+
+export default App
+
+const Root = () => {
+  // Check if token exists in localstorage
+  const isAuthenticated = !!localStorage.getItem("token");
+
+  //Redirect to dashboard if Authenticated, otherwise to login
+  return isAuthenticated ? (
+    <Navigate to="/dashboard" />
+  ) : (
+    <Navigate to="/login" />
+  );
+};
