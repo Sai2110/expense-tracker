a80e63270dce3ccfd3bc32c073d035061e3b28cb
ArthurLewin <117980762+Sai2110@users.noreply.github.com>
2025-07-09 16:30:27 +0530

    Update apiPaths.js

--- frontend/expense-tracker/src/utils/apiPaths.js
+++ frontend/expense-tracker/src/utils/apiPaths.js
@@ -1,4 +1,5 @@
-export const BASE_URL = "http://localhost:8000";
+//export const BASE_URL = "http://localhost:8000"; // this is for local run
+export const BASE_URL = "https://expense-tracker-rka8.onrender.com"; // for deploying
 
 //utils/apiPaths.js
 export const API_PATHS ={
